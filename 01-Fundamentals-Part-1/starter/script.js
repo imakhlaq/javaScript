88cc4f401ac2f906131a7360125a0df4e92d3d57
imakhlaq <imakhlaq@gmail.com>
2022-08-09 14:06:58 +0530

    initial

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,52 @@
+// let js = "amazing";
+// let MName = "akhlaq Ahmad";
+// console.log(MName);
+// console.log(MName);
+// console.log(MName);
+
+// let myCountry = "india";
+// let myContinet = "Asia";
+// let population = "180M";
+// console.log(myCountry);
+// console.log(myContinet);
+// console.log(population);
+
+// let javasScriptIsFun = true;
+// console.log(typeof javasScriptIsFun);
+// console.log(javasScriptIsFun);
+// javasScriptIsFun = "funnnn";
+// console.log(typeof javasScriptIsFun);
+// console.log(javasScriptIsFun);
+
+// console.log(typeof null);
+
+// const birthDate = 2001;
+// const currentYear = 2022;
+// const myAge = currentYear - birthDate;
+// console.log(myAge);
+
+// coding challange
+
+let markMass = 78;
+let johnMass = 92;
+
+let markHeight = 1.69;
+let johnHeight = 1.95;
+
+let markBmi = markMass / (markHeight * markHeight);
+let johnBmi = johnMass / (johnHeight * johnHeight);
+console.log(markBmi, johnBmi);
+
+const markBmiHigher = markBmi > johnBmi;
+console.log(markBmiHigher);
+
+markMass = 95;
+johnMass = 85;
+
+markHeight = 1.69;
+johnHeight = 1.88;
+
+markBmi = markMass / (markHeight * markHeight);
+johnBmi = johnMass / (johnHeight * johnHeight);
+const johnBmiHigher = markBmi < johnBmi;
+console.log(johnBmiHigher);
