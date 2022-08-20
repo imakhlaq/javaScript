346731a9ed4d7f9f83c0b7242e5b24070648a01d
imakhlaq <imakhlaqXD@gmail.com>
2022-08-21 00:42:42 +0530

    challenge 3

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -199,3 +199,11 @@
 // console.log(drink);
 
 // console.log(`I like to drink ${age >= 18 ? "water" : "juice"}`);
+
+//coding challenge 4
+const bill = 275;
+
+const tip = bill >= 50 && bill <= 300 ? (15 / 100) * bill : (20 / 100) * bill;
+console.log(
+  `The bill was ${bill}, the tip was ${tip} and total value ${bill + tip}`
+);
