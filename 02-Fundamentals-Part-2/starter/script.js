16b907ce9632cae062cc3be7165261f6c71647f6
imakhlaq <imakhlaqXD@gmail.com>
2022-10-23 20:57:27 +0530

    functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -20,15 +20,31 @@
 // console.log(juice);
 
 //arrow function
-const fruitcuter = (fruite) => fruite * 3;
+// const fruitcuter = (fruite) => fruite * 3;
 
-//function calling another function
-const juiceMaker = function (apple, banana) {
-  //first cut the apple and bananas
-  const applePieces = fruitcuter(apple);
-  const bananaPieces = fruitcuter(banana);
-  const juice = `This juice contain ${applePieces} apple pices and ${bananaPieces} banana pices`;
-  return juice;
+// //function calling another function
+// const juiceMaker = function (apple, banana) {
+//   //first cut the apple and bananas
+//   const applePieces = fruitcuter(apple);
+//   const bananaPieces = fruitcuter(banana);
+//   const juice = `This juice contain ${applePieces} apple pices and ${bananaPieces} banana pices`;
+//   return juice;
+// };
+
+// console.log(juiceMaker(2, 3));
+
+//
+const calcAvg = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+const qulaAvg = calcAvg(85, 54, 41);
+const dolAvg = calcAvg(23, 34, 27);
+const checkWinner = function (qulaAvg, dolAvg) {
+  if (qulaAvg >= dolAvg * 2) {
+    return `Koalas won by ${qulaAvg} vs ${dolAvg}`;
+  } else if (dolAvg >= qulaAvg * 2) {
+    return `Dolphins won by ${dolAvg} vs ${qulaAvg}`;
+  } else {
+    return `Draw No Winner`;
+  }
 };
 
-console.log(juiceMaker(2, 3));
+console.log(checkWinner(qulaAvg, dolAvg));
