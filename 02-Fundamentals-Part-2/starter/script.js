144f5a60ba51dcda5d7e6dfa177cfd97d42ff581
imakhlaq <imakhlaqXD@gmail.com>
2022-10-23 22:09:31 +0530

    arrays

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -34,17 +34,41 @@
 // console.log(juiceMaker(2, 3));
 
 //
-const calcAvg = (score1, score2, score3) => (score1 + score2 + score3) / 3;
-const qulaAvg = calcAvg(85, 54, 41);
-const dolAvg = calcAvg(23, 34, 27);
-const checkWinner = function (qulaAvg, dolAvg) {
-  if (qulaAvg >= dolAvg * 2) {
-    return `Koalas won by ${qulaAvg} vs ${dolAvg}`;
-  } else if (dolAvg >= qulaAvg * 2) {
-    return `Dolphins won by ${dolAvg} vs ${qulaAvg}`;
+
+//coding challenge function
+// const calcAvg = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+// const qulaAvg = calcAvg(85, 54, 41);
+// const dolAvg = calcAvg(23, 34, 27);
+// const checkWinner = function (qulaAvg, dolAvg) {
+//   if (qulaAvg >= dolAvg * 2) {
+//     return `Koalas won by ${qulaAvg} vs ${dolAvg}`;
+//   } else if (dolAvg >= qulaAvg * 2) {
+//     return `Dolphins won by ${dolAvg} vs ${qulaAvg}`;
+//   } else {
+//     return `Draw No Winner`;
+//   }
+// };
+
+// console.log(checkWinner(qulaAvg, dolAvg));
+
+//coding challenge arrays
+
+const tipCal = function (amount) {
+  let tip = 0;
+  if (amount >= 50 && amount <= 300) {
+    tip = (15 / amount) * 100;
   } else {
-    return `Draw No Winner`;
+    tip = (20 / amount) * 100;
   }
+  return tip;
 };
 
-console.log(checkWinner(qulaAvg, dolAvg));
+const bills = [125, 555, 44];
+const tipValue = [tipCal(bills[0]), tipCal(bills[1]), tipCal(bills[2])];
+const total = [
+  bills[0] + tipValue[0],
+  bills[1] + tipValue[1],
+  bills[2] + tipValue[2],
+];
+
+console.log(total);
