8ed20fd29a0d10d66676043d7f636ed1629a138b
imakhlaq <imakhlaqXD@gmail.com>
2022-11-07 04:50:59 +0530

    challenge

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+// const flight =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
 const restaurant = {
@@ -117,19 +117,73 @@ const gameEvents = new Map([
   [92, '🔶 Yellow card'],
 ]);
 
-const events = [...gameEvents.values()];
-const nevents = [...new Set(events)];
+// const events = [...gameEvents.values()];
+// const nevents = [...new Set(events)];
 
-gameEvents.delete(64);
+// gameEvents.delete(64);
 
-const time = [...gameEvents.keys()].pop();
+// const time = [...gameEvents.keys()].pop();
 
-console.log(`An anverage event happens in ${time / gameEvents.size}`);
+// console.log(`An anverage event happens in ${time / gameEvents.size}`);
 
-for (const [key, value] of gameEvents) {
-  if (key <= 45) {
-    console.log(`[FIRST HALF] ${key} : ${value}`);
-  } else {
-    console.log(`[SECOND HALF] ${key} : ${value}`);
-  }
+// for (const [key, value] of gameEvents) {
+//   if (key <= 45) {
+//     console.log(`[FIRST HALF] ${key} : ${value}`);
+//   } else {
+//     console.log(`[SECOND HALF] ${key} : ${value}`);
+//   }
+// }
+
+// const s = 'akhlaq ram sam keppa';
+
+// const cap = function (name) {
+//   const arr = s.split(' ');
+//   const newarr = [];
+
+//   for (const n of arr) {
+//     newarr.push(n.replace(n[0], n[0].toUpperCase()));
+//   }
+
+//   return newarr.join(' ');
+// };
+// console.log(cap(s));
+
+// challenge
+// document.body.append(document.createElement('textarea'));
+// document.body.append(document.createElement('button'));
+
+// const btn = document.querySelector('button');
+
+// btn.addEventListener('click', () => {
+//   const text = document.querySelector('textarea').value;
+//   console.log(text);
+//   const text1 = text.toLowerCase();
+//   let [first, second] = text1.split('_');
+
+//   second = second.replace(second[0], second[0].toUpperCase());
+
+//   console.log(`${first}${second}`);
+// });
+
+// challenge
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+for (const flight of flights.split('+')) {
+  const each = flight.split(';');
+  const [type, from, to, time] = each;
+
+  const output = `${type.includes('Delayed') ? '🔴' : ''} ${type.replaceAll(
+    '_',
+    ' '
+  )} ${from.slice(0, 3).toUpperCase()} ${to
+    .slice(0, 3)
+    .toUpperCase()} (${time.replace(':', 'h')})`.padStart(37);
+
+  console.log(output);
 }
